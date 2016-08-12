cb9c4e395c9cc1ea6fcb7b8c2291f37ce70267e3
William Austin <quikkian@email.arizona.edu>
2016-08-12 16:06:56 -0700

    Initial Commit

---
+++ src/main/resources/urls.js
@@ -0,0 +1,4 @@
+var g_ContentsURL = "KFS%20User%20Documentation-toc.htm";
+var g_IndexURL = "KFS%20User%20Documentation-index.htm";
+var g_SearchURL = "KFS%20User%20Documentation-search.htm";
+var g_FavoritesURL = "KFS%20User%20Documentation-favorites.htm";
